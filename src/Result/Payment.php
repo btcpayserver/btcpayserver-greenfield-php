71df77ba737e79d950601c86bcd5f51850271f3c
Judith <judithdemets@gmail.com>
2021-09-17 14:51:10 +0200

    created Payment class - added getters to Payment & PaymentMethod

---
+++ src/Result/Payment.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BTCPayServer\Result;
+
+class Payment extends AbstractResult
+{
+    public function getValue(): string
+    {
+        $data = $this->getData();
+        return $data['value'];
+    }
+
+    public function getFee(): string
+    {
+        $data = $this->getData();
+        return $data['fee'];
+    }
+
+    public function getDestination(): string
+    {
+        $data = $this->getData();
+        return $data['destination'];
+    }
+
+    public function getStatus(): string
+    {
+        $data = $this->getData();
+        return $data['status'];
+    }
+
+}
