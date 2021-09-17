3235edbf1e42958d468e4466cfd78651a02073a6
Judith <judithdemets@gmail.com>
2021-09-17 14:51:10 +0200

    created Payment class - added getters to Payment & PaymentMethod

---
+++ src/Result/PaymentMethod.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BTCPayServer\Result;
+
+class PaymentMethod extends AbstractResult
+{
+    /**
+     * @return Payment[]
+     */
+    public function getPayments()
+    {
+
+        $r = [];
+        $data = $this->getData();
+        foreach ($data['payments'] as $payment) {
+            $r[] = new \BTCPayServer\Result\Payment($payment);
+        }
+
+        return $r;
+    }
+
+    public function getDestination() :string
+    {
+        $data = $this->getData();
+        return $data['destination'];
+    }
+
+    public function getRate() :string
+    {
+        $data = $this->getData();
+        return $data['rate'];
+    }
+
+    public function getPaymentMethodPaid() :string
+    {
+        $data = $this->getData();
+        return $data['paymentMethodPaid'];
+    }
+
+    public function getTotalPaid() :string
+    {
+        $data = $this->getData();
+        return $data['totalPaid'];
+    }
+
+    public function getDue() :string
+    {
+        $data = $this->getData();
+        return $data['due'];
+    }
+
+    public function getAmount() :string
+    {
+        $data = $this->getData();
+        return $data['amount'];
+    }
+
+    public function getNetworkFee() :string
+    {
+        $data = $this->getData();
+        return $data['networkFee'];
+    }
+
+    public function getPaymentMethod() :string
+    {
+        $data = $this->getData();
+        return $data['paymentMethod'];
+    }
+}
