9bc92c57c9bbeb4dca2cd5d548f034d688e92a6e
Judith <judithdemets@gmail.com>
2021-09-21 17:09:09 +0200

    renamed renamed Payment(Method) to InvoicePayment(Method)

--- src/Result/InvoicePaymentMethod.php
+++ src/Result/InvoicePaymentMethod.php
@@ -6,4 +6,65 @@ namespace BTCPayServer\Result;
 
 class InvoicePaymentMethod extends AbstractResult
 {
+    /**
+     * @return InvoicePayment[]
+     */
+    public function getPayments(): array
+    {
+        $r = [];
+        $data = $this->getData();
+        foreach ($data['payments'] as $payment) {
+            $r[] = new \BTCPayServer\Result\InvoicePayment($payment);
+        }
+
+        return $r;
+    }
+
+    public function getDestination(): string
+    {
+        $data = $this->getData();
+        return $data['destination'];
+    }
+
+    public function getRate(): string
+    {
+        $data = $this->getData();
+        return $data['rate'];
+    }
+
+    public function getPaymentMethodPaid(): string
+    {
+        $data = $this->getData();
+        return $data['paymentMethodPaid'];
+    }
+
+    public function getTotalPaid(): string
+    {
+        $data = $this->getData();
+        return $data['totalPaid'];
+    }
+
+    public function getDue(): string
+    {
+        $data = $this->getData();
+        return $data['due'];
+    }
+
+    public function getAmount(): string
+    {
+        $data = $this->getData();
+        return $data['amount'];
+    }
+
+    public function getNetworkFee(): string
+    {
+        $data = $this->getData();
+        return $data['networkFee'];
+    }
+
+    public function getPaymentMethod(): string
+    {
+        $data = $this->getData();
+        return $data['paymentMethod'];
+    }
 }
