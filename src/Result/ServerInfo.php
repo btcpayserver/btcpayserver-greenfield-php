7eda5c1e9465c419debf78e3768f427d3f0ebd20
Wouter Samaey <wouter.samaey@storefront.be>
2021-10-26 08:58:56 +0200

    CS fix

--- src/Result/ServerInfo.php
+++ src/Result/ServerInfo.php
@@ -6,25 +6,29 @@ namespace BTCPayServer\Result;
 
 class ServerInfo extends AbstractResult
 {
-    public function getVersion(): string{
+    public function getVersion(): string
+    {
         return $this->getData()['version'];
     }
 
     /**
      * @return string
      */
-    public function getOnionUrl(): string{
+    public function getOnionUrl(): string
+    {
         return $this->getData()['onion'];
     }
 
-    public function isFullySynced(): bool{
+    public function isFullySynced(): bool
+    {
         return $this->getData()['fullySynched'];
     }
 
     /**
      * @return string[] Example: ['BTC', 'BTC_LightningLike']
      */
-    public function getSupportedPaymentMethods(): array{
+    public function getSupportedPaymentMethods(): array
+    {
         return $this->getData()['supportedPaymentMethods'];
     }
 
