bb04f613f0970d1f40015995635671de0ddcd832
barrydeen <barry.deen@gmail.com>
2022-01-28 16:10:02 -0500

    Added Get, Archive, Approve, Cancel and Mark methods

--- src/Client/Payout.php
+++ src/Client/Payout.php
@@ -6,6 +6,33 @@ namespace BTCPayServer\Client;
 
 class Payout extends AbstractClient
 {
+    public function getStorePullPayments(
+        string $storeId,
+        bool $includeArchived
+    )
+    {
+        $url = $this->getApiUrl() . 'stores/' . urlencode(
+            $storeId
+        ) . '/pull-payments';
+        $headers = $this->getRequestHeaders();
+        $method = 'GET';
+
+        $body = json_encode(
+            [
+                    'includeArchived' => $includeArchived,
+                ],
+            JSON_THROW_ON_ERROR
+        );
+
+        $response = $this->getHttpClient()->request($method, $url, $headers, $body);
+
+        if ($response->getStatus() === 200) {
+            return json_decode($response->getBody(), true, 512, JSON_THROW_ON_ERROR);
+        } else {
+            throw $this->getExceptionByStatusCode($method, $url, $response);
+        }
+    }
+
     public function createPullPayment(
         string $storeId,
         string $paymentName,
@@ -16,7 +43,7 @@ class Payout extends AbstractClient
         mixed $startsAt,
         mixed $expiresAt,
         array $paymentMethods
-    )
+    ) : Payout
     {
         $url = $this->getApiUrl() . 'stores/' . urlencode(
             $storeId
@@ -39,6 +66,69 @@ class Payout extends AbstractClient
         );
 
         $response = $this->getHttpClient()->request($method, $url, $headers, $body);
+
+        if ($response->getStatus() === 200) {
+            return json_decode($response->getBody(), true, 512, JSON_THROW_ON_ERROR);
+        } else {
+            throw $this->getExceptionByStatusCode($method, $url, $response);
+        }
+    }
+
+    public function archivePullPayment(
+        string $storeId,
+        string $pullPaymentId
+    )
+    {
+        $url = $this->getApiUrl() . 'stores/' . urlencode(
+            $storeId
+        ) . '/pull-payments/' . $pullPaymentId;
+
+        $headers = $this->getRequestHeaders();
+        $method = 'DELETE';
+
+        $response = $this->getHttpClient()->request($method, $url, $headers);
+
+        if ($response->getStatus() === 200) {
+            return json_decode($response->getBody(), true, 512, JSON_THROW_ON_ERROR);
+        } else {
+            throw $this->getExceptionByStatusCode($method, $url, $response);
+        }
+    }
+
+    public function cancelPayout(
+        string $storeId,
+        string $payoutId
+    )
+    {
+        $url = $this->getApiUrl() . 'stores/' . urlencode(
+            $storeId
+        ) . '/payouts/' . $payoutId;
+        
+        $headers = $this->getRequestHeaders();
+        $method = 'DELETE';
+
+        $response = $this->getHttpClient()->request($method, $url, $headers);
+
+        if ($response->getStatus() === 200) {
+            return json_decode($response->getBody(), true, 512, JSON_THROW_ON_ERROR);
+        } else {
+            throw $this->getExceptionByStatusCode($method, $url, $response);
+        }
+    }
+
+    public function markPayoutAsPaid(
+        string $storeId,
+        string $payoutId
+    )
+    {
+        $url = $this->getApiUrl() . 'stores/' . urlencode(
+            $storeId
+        ) . '/payouts/' . $payoutId;
+        
+        $headers = $this->getRequestHeaders();
+        $method = 'POST';
+
+        $response = $this->getHttpClient()->request($method, $url, $headers);
 
         if ($response->getStatus() === 200) {
             return json_decode($response->getBody(), true, 512, JSON_THROW_ON_ERROR);
