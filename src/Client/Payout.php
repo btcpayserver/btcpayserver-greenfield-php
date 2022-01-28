b9cb4b200eb404cda681244e0ea1dc54a934dfed
barrydeen <barry.deen@gmail.com>
2022-01-28 13:44:42 -0500

    Added Pull-Payments/Payout Methods

---
+++ src/Client/Payout.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\BTCPay\Client;
+
+class Payout extends AbstractClient
+{
+    public function createPullPayment(
+        string $storeId,
+        string $paymentName,
+        string $paymentAmount,
+        string $paymentCurrency,
+        mixed $paymentPeriod,
+        mixed $boltExpiration,
+        mixed $startsAt,
+        mixed $expiresAt,
+        array $paymentMethods)
+        {
+            $url = $this->getApiUrl() . 'stores/' . urlencode(
+                $storeId
+            ) . '/pull-payments';
+            $headers = $this->getRequestHeaders();
+            $method = 'POST';
+
+            $body = json_encode(
+                [
+                    'name' => $paymentName,
+                    'amount' => $paymentAmount,
+                    'currency' => $paymentCurrency,
+                    'period' => $paymentPeriod,
+                    'BOLT11Expiration' => $boltExpiration,
+                    'startsAt' => $startsAt,
+                    'expiresAt' => $expiresAt,
+                    'paymentMethods' => $paymentMethods
+                ],
+                JSON_THROW_ON_ERROR
+            );
+
+            $response = $this->getHttpClient()->request($method, $url, $headers, $body);
+
+            if ($response->getStatus() === 200) {
+                return json_decode($response->getBody(), true, 512, JSON_THROW_ON_ERROR);
+            } else {
+                throw $this->getExceptionByStatusCode($method, $url, $response);
+            }
+        }
+    }
