6d6dab81908c6eef62536346ae08620c56d8a90b
barrydeen <barry.deen@gmail.com>
2022-01-28 17:13:40 -0500

    Added Sample Use Cases for Each Method

---
+++ examples/payout_usage.php
@@ -0,0 +1,108 @@
+<?
+
+use BTCPayServer\Client\Payout;
+
+class Payouts {
+
+    public function getStorePullPayments()
+    {
+        $apiKey = '';
+        $host = '';
+        $storeId = '';
+        $includeArchived = TRUE;
+
+        try {
+            $client = new Payout($host, $apiKey);
+            var_dump($client->getStorePullPayments(
+                                $storeId, 
+                                $includeArchived));
+        } catch (\Throwable $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
+    
+    public function createPullPayment()
+    {
+        $apiKey = '';
+        $host = '';
+        $storeId = '';
+        $paymentName = 'TestPayout-' . rand(0,10000000);
+        $paymentAmount = '0.000001';
+        $paymentCurrency = 'BTC';
+        $paymentPeriod = NULL;
+        $boltExpiration = 1;
+        $startsAt = NULL;
+        $expiresAt = NULL;
+        $paymentMethods = ['BTC'];
+
+        try {
+            $client = new Payout($host, $apiKey);
+            var_dump($client->createPullPayment(
+                                $storeId, 
+                                $paymentName, 
+                                $paymentAmount, 
+                                $paymentCurrency, 
+                                $paymentPeriod, 
+                                $boltExpiration, 
+                                $startsAt, 
+                                $expiresAt, 
+                                $paymentMethods)
+            );
+        } catch (\Throwable $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
+
+    public function archivePullPayment()
+    {
+        $apiKey = '';
+        $host = '';
+        $storeId = '';
+        $pullPaymentId = '';
+
+        try {
+            $client = new Payout($host, $apiKey);
+            var_dump($client->archivePullPayment(
+                                $storeId, 
+                                $pullPaymentId));
+        } catch (\Throwable $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
+
+    public function cancelPayout()
+    {
+        $apiKey = '';
+        $host = '';
+        $storeId = '';
+        $payoutId = '';
+
+        try {
+            $client = new Payout($host, $apiKey);
+            var_dump($client->cancelPayout(
+                                $storeId, 
+                                $payoutId));
+        } catch (\Throwable $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
+
+    public function markPayoutAsPaid()
+    {
+        $apiKey = '';
+        $host = '';
+        $storeId = '';
+        $payoutId = '';
+
+        try {
+            $client = new Payout($host, $apiKey);
+            var_dump($client->markPayoutAsPaid(
+                                $storeId, 
+                                $payoutId));
+        } catch (\Throwable $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
+}
+
+
