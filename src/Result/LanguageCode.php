5750780de22065fe768696a251c6b71ffd55e9ce
barrydeen <barry.deen@gmail.com>
2022-02-10 16:17:18 -0500

    Rebase Branch, Added back in Getters

--- src/Result/LanguageCode.php
+++ src/Result/LanguageCode.php
@@ -6,4 +6,15 @@ namespace BTCPayServer\Result;
 
 class LanguageCode extends AbstractResult
 {
+    public function getCode(): string
+    {
+        $data = $this->getData();
+        return $data['code'];
+    }
+
+    public function getCurrentLanguage(): string
+    {
+        $data = $this->getData();
+        return $data['currentLanguage'];
+    }
 }
--- src/Result/Notification.php
+++ src/Result/Notification.php
@@ -6,4 +6,33 @@ namespace BTCPayServer\Result;
 
 class Notification extends AbstractResult
 {
+    public function getId(): string
+    {
+        $data = $this->getData();
+        return $data['id'];
+    }
+
+    public function getBody(): string
+    {
+        $data = $this->getData();
+        return $data['body'];
+    }
+
+    public function getLink(): string
+    {
+        $data = $this->getData();
+        return $data['link'];
+    }
+
+    public function getCreatedTime(): int
+    {
+        $data = $this->getData();
+        return $data['createdTime'];
+    }
+
+    public function isSeen(): bool
+    {
+        $data = $this->getData();
+        return $data['seen'];
+    }
 }
