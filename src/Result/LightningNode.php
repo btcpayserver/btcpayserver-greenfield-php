87906b1d31ad119bb84936919ba2e119bcc31a97
barrydeen <barry.deen@gmail.com>
2022-02-10 13:51:11 -0500

    Merged only getters and not client and usage files

---
+++ src/Result/LightningNode.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BTCPayServer\Result;
+
+class LightningNode extends AbstractResult
+{
+    /**
+     * @return array strings
+     */
+    public function getNodeURIs(): array
+    {
+        $data = $this->getData();
+        return $data['nodeURIs'];
+    }
+
+    public function getBlockHeight(): int
+    {
+        $data = $this->getData();
+        return $data['blockheight'];
+    }
+}
