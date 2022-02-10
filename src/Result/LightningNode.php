d3cce2c285b1f5999a53a6f0fef2cbff24f902d0
barrydeen <barry.deen@gmail.com>
2022-02-10 16:17:18 -0500

    Rebase Branch, Added back in Getters

--- src/Result/LightningNode.php
+++ src/Result/LightningNode.php
@@ -6,4 +6,18 @@ namespace BTCPayServer\Result;
 
 class LightningNode extends AbstractResult
 {
+    /**
+     * @return array strings
+     */
+    public function getNodeURIs(): array
+    {
+        $data = $this->getData();
+        return $data['nodeURIs'];
+    }
+
+    public function getBlockHeight(): int
+    {
+        $data = $this->getData();
+        return $data['blockheight'];
+    }
 }
--- src/Result/StoreOnChainWallet.php
+++ src/Result/StoreOnChainWallet.php
@@ -4,6 +4,25 @@ declare(strict_types=1);
 
 namespace BTCPayServer\Result;
 
+use BTCPayServer\Util\PreciseNumber;
+
 class StoreOnChainWallet extends AbstractResult
 {
+    public function getBalance(): PreciseNumber
+    {
+        $data = $this->getData();
+        return PreciseNumber::parseString($data['balance']);
+    }
+
+    public function getUnconfirmedBalance(): PreciseNumber
+    {
+        $data = $this->getData();
+        return PreciseNumber::parseString($data['unconfirmedBalance']);
+    }
+
+    public function getConfirmedBalance(): PreciseNumber
+    {
+        $data = $this->getData();
+        return PreciseNumber::parseString($data['confirmedBalance']);
+    }
 }
