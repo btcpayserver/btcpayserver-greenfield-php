d02579c5120d83136b8247d903002b82c0acdf91
barrydeen <barry.deen@gmail.com>
2022-02-09 19:48:44 -0500

    Added Miscellaneous Methods, Results, Usage

---
+++ examples/miscellaneous.php
@@ -0,0 +1,56 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use BTCPayServer\Client\Miscellaneous;
+
+class Misc {
+
+    public $apiKey;
+    public $host;
+    public $storeId;
+
+    public function __construct()
+    {   
+        $this->apiKey = '';
+        $this->host = '';
+    }
+
+    public function getPermissionsMetadata()
+    {
+        try {
+            $client = new Miscellaneous($this->host, $this->apiKey);
+            var_dump($client->getPermissionsMetadata());
+        } catch (\Throwable $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
+
+    public function getLanguageCodes()
+    {
+        try {
+            $client = new Miscellaneous($this->host, $this->apiKey);
+            var_dump($client->getLanguageCodes());
+        } catch (\Throwable $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
+
+    public function getInvoiceCheckout()
+    {
+        $invoiceId = 'asdfasdfasdf';
+        $lang = 'en';
+
+        try {
+            $client = new Miscellaneous($this->host, $this->apiKey);
+            var_dump($client->getInvoiceCheckout($invoiceId, $lang));
+        } catch (\Throwable $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
+}
+
+$misc = new Misc();
+//$misc->getPermissionsMetadata();
+//$misc->getLanguageCodes();
+//$misc->getInvoiceCheckout();
