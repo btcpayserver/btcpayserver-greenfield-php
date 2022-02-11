7c98e5532bf49e27a9acad1e6d50a2681d5499bc
barrydeen <barry.deen@gmail.com>
2022-02-11 18:04:21 -0500

    Webhook Getters and Missing Methods

--- src/Result/Webhook.php
+++ src/Result/Webhook.php
@@ -4,6 +4,43 @@ declare(strict_types=1);
 
 namespace BTCPayServer\Result;
 
+use BTCPayServer\Result\WebhookAuthorizedEventList;
+
 class Webhook extends AbstractResult
 {
+    public function getId(): string
+    {
+        $data = $this->getData();
+        return $data['id'];
+    }
+
+    public function isEnabled(): bool
+    {
+        $data = $this->getData();
+        return $data['enabled'];
+    }
+
+    public function hasAutomaticRedelivery(): bool
+    {
+        $data = $this->getData();
+        return $data['automaticRedelivery'];
+    }
+
+    public function getUrl(): string
+    {
+        $data = $this->getData();
+        return $data['url'];
+    }
+
+    public function getAuthorizedEvents(): WebhookAuthorizedEventList
+    {
+        $data = $this->getData();
+        return new WebhookAuthorizedEventList($data['authorizedEvents']);
+    }
+
+    public function getSecret(): string
+    {
+        $data = $this->getData();
+        return $data['secret'];
+    }
 }
