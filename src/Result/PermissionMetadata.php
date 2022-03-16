d5192c2d43a8eed004b89830e13d694bb3726791
barrydeen <barry.deen@gmail.com>
2022-03-16 14:40:28 -0400

    psr-4

--- src/Result/PermissionMetadata.php
+++ src/Result/PermissionMetadata.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace BTCPayServer\Result;
 
-class PermissionMetaData extends AbstractResult
+class PermissionMetadata extends AbstractResult
 {
     public function getName(): string
     {
