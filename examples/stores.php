74423a5e909e32a2a2f211f883c7376a5202c089
ndeet <andy.tasch@gmail.com>
2024-04-02 23:38:17 +0200

    Adding missing functions of updating and deleting store; renaming examples file.

--- examples/stores.php
+++ examples/stores.php
@@ -8,9 +8,10 @@ use BTCPayServer\Client\Store;
 $apiKey = '';
 $host = ''; // e.g. https://your.btcpay-server.tld
 $storeId = '';
-$invoiceId = '';
+$updateStoreId = '';
 
 // Get information about store on BTCPay Server.
+
 try {
     $client = new Store($host, $apiKey);
     var_dump($client->getStore($storeId));
@@ -21,7 +22,17 @@ try {
 // Create a new store.
 try {
     $client = new Store($host, $apiKey);
-    var_dump($client->createStore('my new store'));
+    $newStore = $client->createStore('New store', null, 'EUR');
+    var_dump($newStore);
+} catch (\Throwable $e) {
+    echo "Error: " . $e->getMessage();
+}
+
+// Update a store.
+// You need to pass all variables to make sure it does not get reset to defaults if you want to preserve them.
+try {
+    $client = new Store($host, $apiKey);
+    var_dump($client->updateStore($updateStoreId, 'Store name CHANGED'));
 } catch (\Throwable $e) {
     echo "Error: " . $e->getMessage();
 }
